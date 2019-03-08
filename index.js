37ebf08f479d18dc8c55789df005d183d829acf5
Maxim Tereshchenko <maxter@Maxims-MacBook-Air.local>
2019-03-08 20:26:18 +0200

    The request part is done

--- index.js
+++ index.js
@@ -12,56 +12,102 @@
 	// 	return filteredLink[0].href
 	// }
 	//
-	// function getFBid(user) {
-	// 	const url = "https://findmyfbid.in/apiv1/";
-	// 	const TOKEN = "68aaacffca53ad8022ebdd28299f195044a259f0";
-	// 	fetch(url, {
-	// 	    method : "POST",
-	// 		headers: `Authorization: Token ${TOKEN}`,
-	// 	    // body: new FormData(document.getElementById("inputform")),
-	// 	    // -- or --
-	// 	    body : JSON.stringify({
-	// 	        fburl : user
-	// 	    })
-	// 	}).then(
-	// 	    response => response.text() // .json(), etc.
-	// 	    // same as function(response) {return response.text();}
-	// 	).then(
-	// 	    html => console.log(html)
-	// 	);
-	// }
-	//
-	// async function mainFunc () {
-	// 	let link = await find_link_by_href();
-	// 	getFBid(link);
-	// }
+	var user;
 
-	function whenAvailable(name, callback) {
+	function sendBroadcast(user, blockname) {
+		const url = 'http://localhost:5000/broadcast';//TODO check the URI
+		fetch(url, {
+		    method : "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({"pageid": user})
+		}).then(
+		    response => {
+		    	if (response.ok) {
+					// you can call response.json() here too if you want to return json
+					console.log('everything is OK')
+					//TODO change the color or smth
+				} else {
+					//handle errors in the way you want to
+					switch (response.status) {
+						case 404:
+							console.log('404 - PSID has not found in the Database');
+							//TODO change the color or smth
+							break;
+						case 400:
+							console.log('400 - server could not understand the request due to invalid syntax');
+							//TODO change the color or smth
+							break;
+						case 500:
+							console.log('500 - Internal server error');
+							//TODO change the color or smth
+							break;
+						default:
+							console.log('Some error occured');
+							//TODO change the color or smth
+							break;
+					}}
+			} // .json(), etc.
+		).then(
+		    res => console.log(res)
+		).catch(err => console.log(err));
+	}
+
+	function createArea() {
+		const area = document.querySelector('#content');
+		const div = document.createElement('div');
+		const button = document.createElement('button');
+		const button2 = document.createElement('button');
+		const textnode = document.createTextNode('Send 1st block');
+		const textnode2 = document.createTextNode('Send 2nd block');
+		div.setAttribute("id", "area");
+		button.setAttribute("class", "btn-chatbotex");
+		button2.setAttribute("class", "btn-chatbotex");
+		area.appendChild(div);
+		div.appendChild(button);
+		div.appendChild(button2);
+		button.appendChild(textnode);
+		button2.appendChild(textnode2);
+	}
+
+	//helper func
+	function whenAvailable(node, callback) {
 		const interval = 100; // ms
 		window.setTimeout(function () {
-			if (window[name]) {
-				callback(window[name]);
+			if (node) {
+				callback(node);
 			} else {
 				window.setTimeout(arguments.callee, interval);
 			}
 		}, interval);
 	}
 
-
-	// window.onload = setTimeout(mainFunc, 10000);//TODO find the better way
-	// window.addEventListener('load', console.log('changed'))
-	//
-	// window.addEventListener('popstate', function (e) {
-	// 	console.log('url changed')
-	// });
-
 	chrome.runtime.onMessage.addListener(
 		function (request, sender, sendResponse) {
 			// listen for messages sent from background.js
 			if (request.message === 'hello!') {
-				console.log(request.url) // new url is now in content scripts!
+				const urlParams = new URLSearchParams(request.url);
+				const myParam = urlParams.get('selected_item_id');
+				console.log(myParam);
+				user = myParam;
 			}
 		});
+
+	window.onload = () => {
+		createArea();
+		const buttons = document.querySelectorAll('.btn-chatbotex');
+		if (buttons.length > 0){
+			const btn1 = buttons[0];
+			const btn2 = buttons[1];
+			btn1.addEventListener('click',()=>{
+				sendBroadcast(user,'broadcast')
+			});
+			btn2.addEventListener('click',()=>{
+				sendBroadcast(user,'test')
+			})
+		}
+	}
 })();
 
-//https://screencast.com/t/dGnFZj0RY0
+
